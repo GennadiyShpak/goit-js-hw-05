93fdada66fd5256c095080958297dc37c35d9b44
Gennadiy <borchuga@gmail.com>
2020-08-16 15:50:47 +0300

    dev version

---
+++ js/task-3.js
@@ -0,0 +1,31 @@
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
+  getItems(items) {
+    return this.items;
+  }
+  addItem(item) {
+    return items.push(item);
+  }
+
+  removeItem(item) {
+    return (this.items = this.items.filter(arrItem => arrItem !== item));
+  }
+}
+
+const storage = new Storage([
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem('Дроид');
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem('Пролонгер');
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
